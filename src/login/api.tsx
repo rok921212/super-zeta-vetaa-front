398aacdc513e930a52c7be0acfead3f6789d14bb
unknown <smitthapa808@gmail.com>
2026-08-21 17:08:40 +0545

    ew

--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://super-zeta-beta-back.onrender.com/api",
+  baseURL: "http://localhost:3000/api",
   headers: {
     'Content-Type': 'application/json',
   }
