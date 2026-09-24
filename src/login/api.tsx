f96f23be01bea38dfc1455a5b32be7d9840b03c8
unknown <smitthapa808@gmail.com>
2026-09-24 15:10:57 +0545

    newew

--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const DEFAULT_BACKEND ="https://super-zeta-beta-back-p2z6.onrender.com";
+export const DEFAULT_BACKEND ="https://super-zeta-beta-back-p22q.onrender.com";
 // Keep in sync with desktop-app/relay/server.cjs RELAY_PORT and
 // src-tauri/src/overlay_relay.rs RELAY_PORT.
 const DEFAULT_RELAY_ORIGIN = "http://127.0.0.1:8787";
